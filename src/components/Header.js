09fc26ea18a9ee733d0973de254fc07c84283260
pranav <katkarpranav4@gmail.com>
2023-12-24 14:56:18 +0530

    built header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute py-2 px-3 bg-gradient-to-b from-black ">
+      <img
+        className=""
+        src="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2023.ico"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
